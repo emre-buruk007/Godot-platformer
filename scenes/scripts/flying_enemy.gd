d96d0faa7128b040486d0107f912d6ada923fcd6
emre-buruk007 <emre.buruk007@gmail.com>
2023-11-13 03:34:14 +0200

    Initial setup of the project

---
+++ scenes/scripts/flying_enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+class_name Enemy
+
+@onready var path_follow = get_parent()
+@onready var path_2d = $"../.."
+@export var speed = 0.15
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# find the current location as % of path completed
+	var current_progress = path_follow.get_progress_ratio()
+	
+	# check if enemy made it to one side, then flip sprite 
+	if (current_progress < 0.5):
+		animated_sprite_2d.flip_h = false
+		path_follow.set_progress_ratio(current_progress + speed * delta)
+	else:
+		animated_sprite_2d.flip_h = true
+		path_follow.set_progress_ratio(current_progress + speed * delta)
+
+	move_and_slide()
+
+func die():
+	queue_free()
---
+++ scenes/scripts/main_character.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	#Animation
+	if (velocity.x > 1 || velocity.x < -1):
+		animated_sprite_2d.play("walking")
+	else:
+		animated_sprite_2d.animation = "idle"
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	# enemy collision kill and death logic
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var collider := collision.get_collider()
+		var is_stomping = (
+			collider is Enemy and is_on_floor()
+			and collision.get_normal().dot(Vector2.UP) > 0.5)
+		
+		# these are collisions with enemy that kill player 
+		var deadly_collision = (
+			collider is Enemy and 
+			collision.get_normal().dot(Vector2.RIGHT)> 0.5 or 
+			collision.get_normal().dot(Vector2.LEFT)> 0.5 or
+			collision.get_normal().dot(Vector2.DOWN)> 0.5 )
+		if is_stomping:
+			velocity.y = JUMP_VELOCITY / 2
+			(collider as Enemy).die()
+	
+	# logic to turn the sprite during movement
+	var isLeft = velocity.x < 0
+	animated_sprite_2d.flip_h = isLeft
