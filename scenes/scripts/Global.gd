71180a2d796a01d8ba58479245eb0776729faffc
emre-buruk007 <emre.buruk007@gmail.com>
2023-11-15 22:55:43 +0200

    Bugfixes and added player-following camera

--- scenes/scripts/Global.gd
+++ scenes/scripts/Global.gd
@@ -3,6 +3,13 @@ extends Node
 
 var score = 0
 
+signal set_camera_target(target: CharacterBody2D)
+
+# This will emit the above signal
+func emit_set_camera_target(target: CharacterBody2D) -> void:
+	set_camera_target.emit(target)
+
+
 # This will stop the engine from processing player physics
 func player_freeze(player: CharacterBody2D) -> void:
 	player.set_physics_process(false)
--- scenes/scripts/main_character.gd
+++ scenes/scripts/main_character.gd
@@ -10,9 +10,7 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-	
-func check_collectible():
-	pass
+
 
 func _physics_process(delta):
 	#Animation
