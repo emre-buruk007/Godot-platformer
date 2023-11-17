e54b08a2a68f9ccd525f24bc117caca9e9351caf
emre-buruk007 <emre.buruk007@gmail.com>
2023-11-17 19:50:04 +0200

    Bugfixing

--- scenes/scripts/GameWinScreen.gd
+++ scenes/scripts/GameWinScreen.gd
@@ -7,7 +7,6 @@ func _ready():
 	bgm.play()
 
 
-
 func _on_button_restart_pressed():
 	Global.score = 0
 	get_tree().change_scene_to_file("res://scenes/Levels/Level_1.tscn")
--- scenes/scripts/game_win.gd
+++ scenes/scripts/game_win.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body):
+	Global.player_freeze(body)
+	next_level.play()
+	await(next_level.finished)
+	get_tree().change_scene_to_file("res://scenes/Levels/level_2.tscn")
