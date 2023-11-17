5b8cc3fe23399aba14f5e11c9011235c3a5c1dd6
emre-buruk007 <emre.buruk007@gmail.com>
2023-11-17 20:14:38 +0200

    Implemented game winning, time to do first upload

--- scenes/scripts/game_win.gd
+++ scenes/scripts/game_win.gd
@@ -1,7 +1,10 @@
 extends Area2D
+@onready var sprite_2d = $Sprite2D
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var game_win = $GameWin
 
 
-# Called when the node enters the scene tree for the first time.
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +12,6 @@ func _ready():
 
 func _on_body_entered(body):
 	Global.player_freeze(body)
-	next_level.play()
-	await(next_level.finished)
-	get_tree().change_scene_to_file("res://scenes/Levels/level_2.tscn")
+	game_win.play()
+	await(game_win.finished)
+	get_tree().change_scene_to_file("res://scenes/game_win_screen.tscn")
